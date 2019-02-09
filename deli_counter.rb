a14a5efc94a2d3061ec647caab543250cac73a0f
Tracy Ngo <tngo1206@gmail.com>
2019-02-09 03:54:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 
-katz_deli = []
+katz_deli = ["Ada", "Katz"]
 
 def line(array_line)
   if array_line.length > 0
@@ -18,4 +18,8 @@ def take_a_number(array_line, name)
   end
 end
 
-def now_serving 
+def now_serving(array_line)
+  array_line.each do |name|
+    puts "Currently, serving #{name}!"
+    array_line.shift
+  
