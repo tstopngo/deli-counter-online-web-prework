7ee86bf673fc5891c3b67cfc6c1fee8bf2ca8629
Tracy Ngo <tngo1206@gmail.com>
2019-02-09 05:20:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,6 +26,6 @@ def now_serving (array_assigned)
     array_line.shift
      end
   else 
-    puts "There is nobody waiting to be served."
+    puts "There is nobody waiting to be served!"
   end
 end
