a6f8e6899e87ccf98f9bfb02a78aa80c82f8b4ee
Tracy Ngo <tngo1206@gmail.com>
2019-02-09 01:36:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(array_names)
+  if array_names == []
+    array_names.each do |place|
+    puts "Your place in line is #{place}"
+  else
+    puts "The line is currently empty"
+  end
+end
+
+take_a_number(array_names,)
+  
