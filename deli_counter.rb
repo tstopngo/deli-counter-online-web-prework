2ba12514518e98ada7659636ac32df8e86af03cf
Tracy Ngo <tngo1206@gmail.com>
2019-02-09 01:45:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,15 +4,16 @@ katz_deli = []
 
 def line(array_line)
   if array_line.length > 0
-    place = 1
-    array_line.each do |name|
-    take_a_number(name)
-    place += 1
+   
   else
     puts "The line is currently empty"
   end
 end
 
-take_a_number(array_line, name)
+def take_a_number(array_line, name)
+    place = 1
+    array_line.each do |name|
+    take_a_number(name)
+    place += 1
   puts "Welcome, #{name}. You are number #{number}"
   
